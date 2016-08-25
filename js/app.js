8dd4c7cc2f0d422db5d491479e3cbd3b9261ccbb
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-08-25 14:18:51 -0500

    subiendo rpta de ejercicio

---
+++ js/app.js
@@ -0,0 +1,3 @@
+var tiempo = parseInt(prompt("Escribe el tiempo que dura la llamada"));
+var costoMinuto = parseInt(prompt("Escribe el costo del número"));
+document.write("El tiempo de llamada: " + tiempo + " minutos y el costo por minuto: " + costoMinuto + " soles, entonces tendrá que pagar: " + tiempo*costoMinuto + " soles.");
